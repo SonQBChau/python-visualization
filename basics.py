1ca41be28023daa3417a8cc0cd4a5123e7d64d8c
Son Chau <soncqb@gmail.com>
2021-07-26 17:30:20 -0500

    Update basics.py

--- basics.py
+++ basics.py
@@ -2,3 +2,18 @@ from collections import Counter
 marriage_ages = [22, 22, 25, 25, 30, 24, 26, 24, 35]  # create a list
 value_counts = Counter(marriage_ages)  # apply the counter functionality
 print(value_counts.most_common())
+
+def add_two_numbers(x, y):  # function header
+    """
+    Takes in two numbers and returns the sum
+    parameters
+        x : str
+            first number
+        y : str
+            second number
+    returns
+        x+y
+    """
+    z = x + y
+    return z  # function return
+print(add_two_numbers(100,5))  # function  call
