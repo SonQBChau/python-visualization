d0609db95ba81d319ec0c33765890324d12f0a83
Son Chau <soncqb@gmail.com>
2021-07-26 17:31:54 -0500

    Update basics.py

--- basics.py
+++ basics.py
@@ -20,3 +20,34 @@ print(add_two_numbers(100,5))  # function  call
 
 y = lambda x, y: x + y  # an anonymous function which takes x and y and input and returns x+y
 print(y(100,5))  # call the function anonymous
+
+# This creates the list
+depths = [1, 5, 3, 6, 4, 7, 10, 12]
+
+# This outputs the first 5 elements. No number before the : implies 0
+first_5_depths = depths[:5]
+
+print("---0---")
+print(first_5_depths)
+
+# You can easily sum
+print("---1---")
+print(sum(depths))
+
+# And take the max
+print("---2---")
+print(max(depths))
+
+# Slicing with a negative starts from the end, so this returns the last element
+print("---3---")
+print(depths[-1])
+
+# This returns the end of the list starting from the second to the end
+# Nothing after the : implies the end of the list
+print("---4---")
+print(depths[-2:])
+
+# This returns the second, third, and forth elements
+# Remember counting starts at zero!
+print("---5---")
+print(depths[2:5])
