ee7cd8b491d7210b527e68e40a5b8f54ca372bd2
Son Chau <soncqb@gmail.com>
2021-07-26 17:51:48 -0500

    Update basics.py

--- basics.py
+++ basics.py
@@ -109,3 +109,33 @@ my_default_dict = defaultdict(int)   # make a default dictionary
 my_default_dict['age'] = 22          # adding a key-value pair
 print(my_default_dict['company'])    # printing the value of the key "company"
 
+##################
+# SETS IN PYTHON #
+##################
+my_set = set()
+my_set.add(1)
+my_set.add(2)
+my_set.add(1)
+# Note that the set only contains a single 1 value
+print("---0---")
+print(my_set)
+
+my_set2 = set()
+my_set2.add(1)
+my_set2.add(2)
+my_set2.add(3)
+my_set2.add(4)
+print("---1---")
+print(my_set2)
+
+# Prints the overlap
+print("---1---")
+print(my_set.intersection(my_set2))
+print("---2---")
+
+# Prints the combination
+print(my_set.union(my_set2))
+
+# Prints the difference (those in my_set but not my_set2)
+print("---3---")
+print(my_set.difference(my_set2))
