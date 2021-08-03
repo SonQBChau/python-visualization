de5a0fcc47e4066d49758759a2b787552154961a
Son Chau <soncqb@gmail.com>
2021-08-02 22:22:34 -0500

    Update group_by_aggregations.py

--- Describing Data/group_by_aggregations.py
+++ Describing Data/group_by_aggregations.py
@@ -1,4 +1,5 @@
 import pandas as pd
+
 def read_csv():
     # Define the column names as a list
     names = ["mpg", "cylinders", "displacement", "horsepower", "weight", "acceleration", "model_year", "origin", "car_name"]
@@ -8,5 +9,26 @@ def read_csv():
 
 df = read_csv() 
 
+# Describing data
 def group_aggregation(df, group_var, agg_var):
-    return 0 # Update function to return the grouped data frame
+    """
+        We need to group the Auto MPG Dataset on the basis of a column. 
+        Then we have to calculate the mean of the grouped data according to another column
+        
+        parameters
+            df: A dataframe containing the dataset in the form of a matrix.
+            group_var: A variable that will group the data
+            agg_var: A variable to aggregate or describe the grouped data with statistics
+            
+        returns
+            dataframe
+    """
+
+    # Grouping the data and taking mean
+    grouped_df = df.groupby([group_var])[agg_var].mean()
+    return grouped_df
+
+
+
+# Calling the function
+print(group_aggregation(read_csv(),"cylinders","mpg"))
