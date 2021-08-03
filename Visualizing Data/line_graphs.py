343ea55b7d736b24653cda659df424575b3690db
Son Chau <soncqb@gmail.com>
2021-08-03 13:44:18 -0500

    Update line_graphs.py

--- Visualizing Data/line_graphs.py
+++ Visualizing Data/line_graphs.py
@@ -1,7 +1,9 @@
 import seaborn as sns            # importing seaborn functionality    
 import pandas as pd
 import matplotlib.pyplot as plt
- 
+'''
+The first parameter is the list or array of x-values and the second parameter is the array of y-values.
+'''
 flights_long=sns.load_dataset("flights")   # importing dataset
 print(flights_long)
 # filtering the dataset to obtain the January records for all years
