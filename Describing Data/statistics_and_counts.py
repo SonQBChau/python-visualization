a3c1b049d3c625a8021967ed5e3008852609730c
Son Chau <soncqb@gmail.com>
2021-08-02 21:22:20 -0500

    Update statistics_and_counts.py

--- Describing Data/statistics_and_counts.py
+++ Describing Data/statistics_and_counts.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
-# Gathering statistics on data 
 names = ['age', 'workclass', 'fnlwgt', 'education', 'educationnum', 'maritalstatus', 'occupation', 'relationship', 'race',
         'sex', 'capitalgain', 'capitalloss', 'hoursperweek', 'nativecountry', 'label']
 train_df = pd.read_csv("./data/adult.data", header=None, names=names)
+# Gathering statistics on data 
 print(train_df.describe())
 
+# Read in data as explained in reading CSV lesson
+print(train_df.info())  # Use the info() function on the dataframe
