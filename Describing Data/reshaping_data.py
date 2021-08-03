91db9b26a7a73ccc70f5f70c29e34585a52109a6
Son Chau <soncqb@gmail.com>
2021-08-02 21:58:16 -0500

    Update reshaping_data.py

--- Describing Data/reshaping_data.py
+++ Describing Data/reshaping_data.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import pandas.util.testing as tm; tm.N = 3
+
 names = ['age', 'workclass', 'fnlwgt', 'education', 'educationnum', 'maritalstatus', 'occupation', 'relationship', 'race',
         'sex', 'capitalgain', 'capitalloss', 'hoursperweek', 'nativecountry', 'label']
 train_df = pd.read_csv("./data/adult.data", header=None, names=names)
@@ -12,3 +14,15 @@ print(pd.pivot_table(train_df, values='hoursperweek', index=['relationship','wor
 # CROSS TAB
 # Calculate the frequencies between label and relationship
 print(pd.crosstab(train_df['label'], train_df.relationship))
+# Crosstab with normalized outputs
+print(pd.crosstab(train_df['label'], train_df.relationship, normalize=True))
+
+# RESHAPE
+def unpivot(frame):
+    N, K = frame.shape
+    data = {'value' : frame.values.ravel('F'),
+            'variable' : np.asarray(frame.columns).repeat(N),
+            'date' : np.tile(np.asarray(frame.index), K)}
+    return pd.DataFrame(data, columns=['date', 'variable', 'value'])
+df = unpivot(tm.makeTimeDataFrame())
+print(df.head())
