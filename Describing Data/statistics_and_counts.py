1c24bcee1910336a3cd361f3eaa87ffd904d1b50
Son Chau <soncqb@gmail.com>
2021-08-02 21:44:28 -0500

    Update statistics_and_counts.py

--- Describing Data/statistics_and_counts.py
+++ Describing Data/statistics_and_counts.py
@@ -4,20 +4,21 @@ names = ['age', 'workclass', 'fnlwgt', 'education', 'educationnum', 'maritalstat
         'sex', 'capitalgain', 'capitalloss', 'hoursperweek', 'nativecountry', 'label']
 train_df = pd.read_csv("./data/adult.data", header=None, names=names)
 
-# Gathering statistics on data 
+# GATHERING STATISTICS ON DATA 
 print(train_df.describe())
 
-# Read in data as explained in reading CSV lesson
+# FINDING THE DATA TYPES
 print(train_df.info())  # Use the info() function on the dataframe
 
-# Converting data types
+# CONVERTING DATA TYPES
 train_df['numeric_column'] = pd.to_numeric(train_df['age'])
 print(train_df.head())
 
-# Finding unique values
+# FINDING UNIQUE VALUES
 print(train_df['relationship'].unique())
 print(train_df['relationship'].value_counts())
 
+# GROUPING THE DATA
 # This function takes a list of columns by which you would like to group your dataframe. 
 # It then performs the requested calculations on each group individually and returns the results by group      
 # Group by relationship and then get the value counts of label with normalization             
@@ -29,9 +30,13 @@ print(train_df.groupby('relationship')['label'].value_counts(normalize=True))
 # Never-worked average about 28 hours
 print(train_df.groupby(['workclass'])['hoursperweek'].mean())
 
-# Finding the correlation
+# FINDING THE CORRELATION
 # there is a higher correlation between “hours per week” and “education num”
 print(train_df.corr())
 # Convert the string label into a value of 1 when >= 50k and 0 otherwise
 train_df['label_int'] = train_df.label.apply(lambda x: ">" in x)
 print(train_df.corr())
+
+# GENERATING PERCENTILES
+# Use the describe function to calculate the percentiles specified                     
+print(train_df.describe(percentiles=[.01,.05,.95,.99]))
