a54203a2997df5fdaa7f736cb06ca20ec4d57f1d
Son Chau <soncqb@gmail.com>
2021-08-02 22:23:17 -0500

    Update reshaping_data.py

--- Describing Data/reshaping_data.py
+++ Describing Data/reshaping_data.py
@@ -6,18 +6,24 @@ names = ['age', 'workclass', 'fnlwgt', 'education', 'educationnum', 'maritalstat
         'sex', 'capitalgain', 'capitalloss', 'hoursperweek', 'nativecountry', 'label']
 train_df = pd.read_csv("./data/adult.data", header=None, names=names)
 
-# PIVOT TABLE
+###############
+# PIVOT TABLE #
+###############
 # Pivot the data frame to show by relationship, workclass (rows) and label (columns) the average hours per week.
 print(pd.pivot_table(train_df, values='hoursperweek', index=['relationship','workclass'], 
                columns=['label'], aggfunc=np.mean).round(2))
 
-# CROSS TAB
+#############
+# CROSS TAB #
+#############
 # Calculate the frequencies between label and relationship
 print(pd.crosstab(train_df['label'], train_df.relationship))
 # Crosstab with normalized outputs
 print(pd.crosstab(train_df['label'], train_df.relationship, normalize=True))
 
-# RESHAPE
+###########
+# RESHAPE #
+###########
 def unpivot(frame):
     N, K = frame.shape
     data = {'value' : frame.values.ravel('F'),
