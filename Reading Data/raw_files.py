d6c159d50152250499d223e33d3b51a8550a6fc8
Son Chau <soncqb@gmail.com>
2021-08-02 20:55:12 -0500

    Update raw_files.py

--- Reading Data/raw_files.py
+++ Reading Data/raw_files.py
@@ -13,3 +13,12 @@ tmp = tempfile.NamedTemporaryFile()
 with open('tmp.name', "r") as f:
     for line in f:
       print(line)
+      
+      
+first_values = []  # Define a list to store the first values of each row
+with open('tmp.name', "r") as f:  # Open the file to read
+    for line in f:  # Loop over each line
+      row_values = line.split("|")  # Split each line by the | character into a list
+      first_values.append(row_values[0])  # Add the first value to our list
+      
+print(first_values)
